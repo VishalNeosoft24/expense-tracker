38f7b2a40254c5c1ae6c4f4575573b97f9102ee3
vishal <vneosoft@gmail.com>
2025-09-02 00:04:19 +0530

    added item_name column in expense

--- expenses/models.py
+++ expenses/models.py
@@ -7,6 +7,7 @@ class Expense(models.Model):
     """Represents a financial expense linked to a category."""
 
     category = models.ForeignKey(Category, related_name='expenses', on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
     expense_date = models.DateField()
--- expenses/serializers.py
+++ expenses/serializers.py
@@ -3,5 +3,5 @@ from .models import Expense
 
 class AddExpenseSerializer(ModelSerializer):
     class Meta:
-        fields = ["category", "amount", "description", "expense_date"]
+        fields = ["category", "item_name", "amount", "description", "expense_date"]
         model = Expense
